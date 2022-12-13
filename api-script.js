2c18a29f46fc6d9bb515955001a1590282ddcb3a
uczcbbl <120495922+uczcbbl@users.noreply.github.com>
2022-12-13 17:48:18 +0000

    Create api-script.js

---
+++ api-script.js
@@ -0,0 +1,21 @@
+var request = new XMLHttpRequest();
+
+request.open("GET", "https://api.giphy.com/v1/gifs/random?api_key=sTdF2n7y8wUcMa7x5uOo9uSdySawdPn3&tag=puppy&rating=g");
+var imageTag = "";
+request.onload = function () {
+  var response = request.reponse;
+  //console.log(response)
+  var parseData = JSON.parse(response);
+  //console.log(parseDate)
+  var imageUrl = parseData.data.data.images.original.url;
+  
+  imageTag = "<img src=" + imageUrl + “>”
+
+	//Add your code here.
+
+
+
+	 document.getElementById("holder").innerHTML = imageTag;
+};
+
+request.send();
